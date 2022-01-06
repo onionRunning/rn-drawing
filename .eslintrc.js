2e8149f7fb08a90fd6093d01c2807b6641542b36
王聪 <wangcong@MacBook-Pro.local>
2022-01-06 19:50:40 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  extends: ['@react-native-community', '5e'],
+  rules: {
+    'react-native/no-inline-styles': 0,
+    '@typescript-eslint/no-require-imports': 1,
+    camelcase: 0,
+    '@typescript-eslint/naming-convention': 0,
+  },
+};
---
+++ README.md
@@ -0,0 +1,19 @@
+# 画板app
+
+## 技术栈
+
+- react-native    0.66.x
+- typescript      4.5.4
+- sqlite
+- react-navigation 6.0.6
+- eslint-config-5e
+- react-native-canvas
+
+## 项目结构
+
+- components 组件模块
+- global 全局模块
+- images 图片静态资源
+- pages 业务页面
+- store 状态管理
+- router 路由管理
